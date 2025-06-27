8a4b11d479cbbe55773136c0001bd3234489e234
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2025-06-27 23:21:06 +0900

    [Feature/issue-001] 기본 레이아웃 구성 (#2)
    * chore: commitlint, husky설정
    
    * chore: tailwind prettier 설정
    
    * feat: root layout font setting
    
    * chore: logo.svg 추가
    
    * design: shadcn sidebar컴포넌트 아이콘, 애니메이션 효과 수정
    
    * feat: 로그인 버튼 생성
    
    * feat: 검색창 생성
    
    * feat: 네비게이션바 생성
    
    * feat: 사이드바 콘텐츠 생성
    
    * feat: 사이드바 생성
    
    * feat: 사이드바, 네비게이션바 적용

---
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
+import Link from "next/link";
+
+const ITEMS = [
+  {
+    title: "History",
+    url: "/playlists/history",
+    icon: HistoryIcon,
+    auth: true,
+  },
+  {
+    title: "Liked videos",
+    url: "/playlists/liked",
+    icon: ThumbsUpIcon,
+    auth: true,
+  },
+  {
+    title: "All playlists",
+    url: "/playlists",
+    icon: ListVideoIcon,
+    auth: true,
+  },
+];
+
+const PersonalSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {ITEMS.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false} // TODO: Change to look at current pathname
+                onClick={() => {}} //TODO: DO something on click
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
+
+export default PersonalSection;
