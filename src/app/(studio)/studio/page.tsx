94bbc9143a4c9242131afbd71cfa4107e0c4b733
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2025-07-16 19:35:58 +0900

    [Feature/issue-001] 스튜디오 비디오 관리 (#13)
    * feat: video 스키마 작성
    
    * feat: studio, video procedures작성
    
    * feat: DEFAULT_LIMIT 상수 설정
    
    * feat: studio에 video목록 프리페칭 및 useInfiniteQuery 적용
    
    * feat: lsyout에 Toast컴포넌트 적용
    
    * feat: video create설정 및 toast설정

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,5 +1,14 @@
-const StudioPage = () => {
-  return <div>studio</div>;
+import { DEFAULT_LIMIT } from "@/constants";
+import StudioView from "@/modules/studio/ui/views/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+const StudioPage = async () => {
+  void trpc.studio.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT });
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 };
 
 export default StudioPage;
