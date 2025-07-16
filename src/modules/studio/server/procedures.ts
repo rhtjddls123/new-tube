1b4fe6dff8105dab2265704b56eadf089294e690
unknown <jjjk0605@naver.com>
2025-07-16 19:16:56 +0900

    feat: studio, video procedures작성

---
+++ src/modules/studio/server/procedures.ts
@@ -0,0 +1,56 @@
+import { db } from "@/db";
+import { videos } from "@/db/schema";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { and, desc, eq, lt, or } from "drizzle-orm";
+import z from "zod";
+
+export const studioRouter = createTRPCRouter({
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updateAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const { cursor, limit } = input;
+      const { id: userId } = ctx.user;
+
+      const data = await db
+        .select()
+        .from(videos)
+        .where(
+          and(
+            eq(videos.userId, userId),
+            cursor
+              ? or(
+                  lt(videos.updatedAt, cursor.updateAt),
+                  and(
+                    eq(videos.updatedAt, cursor.updateAt),
+                    lt(videos.id, cursor.id),
+                  ),
+                )
+              : undefined,
+          ),
+        )
+        .orderBy(desc(videos.updatedAt), desc(videos.id))
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit;
+      const items = hasMore ? data.slice(0, -1) : data;
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updateAt: lastItem.updatedAt,
+          }
+        : null;
+
+      return { items, nextCursor };
+    }),
+});
