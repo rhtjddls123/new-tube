046010ff334bc058074dc018ecfc6ee25be82cda
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2025-07-17 19:55:05 +0900

    [Feature/issue-016] Mux webhook 상세 설정 (#18)
    * chore: 섬네일 placeholder 추가
    
    * feat: 섬네일, 시간, 공개 여부 관련 video 스키마 수정
    
    * feat: 영상 시간 관련 포맷, 제목 관련 util함수 구현
    
    * feat: 자막 관련 video procedures설정 추가
    
    * feat: 섬네일, 프리뷰, 자막 관련 컴포넌트 추가 및 이미지 설정 구현, mux관련 webhook 연동

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatDuration = (duration: number) => {
+  const second = Math.floor((duration % 60000) / 1000);
+  const minutes = Math.floor(duration / 60000);
+  return `${minutes.toString().padStart(2, "0")}:${second.toString().padStart(2, "0")}`;
+};
+
+export const snakeCaseToTitle = (str: string) => {
+  return str.replace(/_/g, " ").replace(/\b\w/g, (char) => char.toUpperCase());
+};
